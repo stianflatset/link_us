8190264a556b7017b9c1b92f12bf1ce1b6498665
Stian <stian.tonning@hotmail.no>
2021-02-23 13:50:40 +0100

    Updating project

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -2,3 +2,12 @@ import 'package:flutter/material.dart';
 //News feed for businesses
 //DELETE THIS COMMENT
 // UI for home site
+
+class home_view extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+    );
+  }
+}
