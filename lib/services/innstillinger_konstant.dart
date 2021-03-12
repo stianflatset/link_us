fc8779a801aeb42b4669938374bb75b25686e094
Flashiota <60142388+Flashiota@users.noreply.github.com>
2021-03-12 06:02:35 +0100

    Lagt til en del valg i meny-menyen

---
+++ lib/services/innstillinger_konstant.dart
@@ -0,0 +1,12 @@
+//Options for the settings menu
+class innstillinger_konstant {
+  static const String DarkMode = 'Dark Mode';
+  static const String Aktivitet = 'Din Aktivitet';
+  static const String Betalinger = 'Bestillinger og Betalinger';
+
+  static const List<String> valg = <String> [
+    DarkMode,
+    Aktivitet,
+    Betalinger
+  ];
+}
--- lib/views_main/menu_view.dart
+++ lib/views_main/menu_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:link_us/services/auth_service.dart';
+import 'package:link_us/services/innstillinger_konstant.dart';
 import 'package:provider/provider.dart';
+import 'package:link_us/services/palette.dart';
 
 // UI for menu
 
@@ -12,15 +14,108 @@ class MenuView extends StatefulWidget {
 class _MenuViewState extends State<MenuView> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child:
-      RaisedButton(
-        child: Text("Logg ut"),
-          onPressed: () {
-        context.read<AuthService>().signOut();
-      }
-      )
+    return Scaffold(
+      body: Center(
+        child: IntrinsicWidth(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              RaisedButton(
+                onPressed: (){},
+                child: Text("Favoritter",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  ),
+                ),
+                color: Palette.buttonColor,
+              ),
+              Divider(
+                height: 15.0,
+                color: Colors.grey,
+                thickness: 0.5,
+              ),
+              RaisedButton(
+                onPressed: (){},
+                child: Text("Personvern",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  ),
+                ),
+                color: Palette.buttonColor,
+              ),
+              Divider(
+                height: 15.0,
+                color: Colors.grey,
+                thickness: 0.5,
+              ),
+              Row(
+                //mainAxisAlignment: MainAxisAlignment.stretch,
+                children: [
+                  PopupMenuButton(
+                    onSelected: choiceAction,
+                    itemBuilder: (BuildContext context) {
+                      return innstillinger_konstant.valg.map((String choice) {
+                        return PopupMenuItem<String>(
+                          value: choice,
+                          child: Text(choice,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18.0,
+                            ),
+                          )
+                        );
+                      }).toList();
+                    },
+                    child: Text("Innstillinger",
+                      style: TextStyle(
+                        backgroundColor: Palette.buttonColor,
+                        color: Colors.white,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    color: Palette.buttonColor,
+                  ),
+                  Icon(Icons.arrow_downward),
+                ],
+              ),
+              Divider(
+                height: 15.0,
+                color: Colors.grey,
+                thickness: 0.5,
+              ),
+              RaisedButton(
+                  onPressed: () {
+                    context.read<AuthService>().signOut();
+                  },
+                child: Text("Logg ut",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  ),
+                ),
+                color: Palette.buttonColor,
+              )
+            ],
+          ),
+        ),
+      ),
+
       
     );
+  }
+
+  void choiceAction(String choice) {
+    if(choice == innstillinger_konstant.DarkMode) {
+     print('DarK mode coming soon');
+    }
+    else if(choice == innstillinger_konstant.Aktivitet) {
+      print('Aktivitet coming soon');
+    }
+    else if(choice == innstillinger_konstant.Betalinger) {
+      print('Betalinger coming soon');
+    }
   }
 }
